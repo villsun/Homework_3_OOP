ddcabb4ab0176e68661e9c06b64c911ee9d45389
villsun <lonukkola@gmail.com>
2024-04-02 19:05:05 +0200

    Add constructors in class Human

--- Lesson_3(homework)/Homework.cpp
+++ Lesson_3(homework)/Homework.cpp
@@ -5,7 +5,43 @@ using namespace std;
 
 class Human
 {
-	
+private:
+	string firstname;
+	string lastname;
+	string surname;
+	int day;
+	int month;
+	int year;
+public:
+	Human()
+	{
+		firstname = "None";
+		lastname = "None";
+		surname = "None";
+		day = -1;
+		month = -1;
+		year = -1;
+	}
+	Human(string firstname, string lastname, string surname, int day, int month, int year)
+	{
+		this->firstname = firstname;
+		this->lastname = lastname;
+		this->surname = surname;
+		this->day = day;
+		this->month = month;
+		this->year = year;
+	}
+	Human(Human& old_human)
+	{
+		firstname = old_human.firstname;
+		lastname = old_human.lastname;
+		surname = old_human.surname;
+		day = old_human.day;
+		month = old_human.month;
+		year = old_human.year;
+	}
+
+
 };
 
 class Apartment
